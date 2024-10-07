824cfac8a32e46aab0ab6b5868448bb3c54a2152
jeevanmanojsai <jeevanmanojsai14@gmail.com>
2024-10-07 12:54:52 +0530

    Uploading code: Donation Rewards.c

---
+++ Donation Rewards.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+    int x;
+    scanf("%d",&x);
+    if(x<=3){
+        printf("BRONZE");
+    }else if (x<=6){
+        printf("SILVER");
+    }else{
+        printf("GOLD");
+
+    }
+}
