1ea03c99be40d2d7380079590c861a6a549e17ee
Bara Jeevan Manoj Sai <jeevanmanojsai14@gmail.com>
2024-12-12 23:49:41 +0530

    Uploading code: Exponential or Quadratic.c

---
+++ Exponential or Quadratic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    if (n == 1) {
+        printf("Yes\n"); 
+    } else if (n == 2) {
+        printf("No\n"); 
+    } else if (n == 3) {
+        printf("No\n");  
+    } else if (n == 4) {
+        printf("No\n");  
+    } else {
+        printf("Yes\n"); 
+    }
+
+    return 0;
+}
